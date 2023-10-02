37e8601ee31c8005d56da096504a630273056b3e
irfiyandaabidin <irfidupang@gmail.com>
2023-10-02 17:05:10 +0700

    form

--- app/src/main/java/com/example/profile/FormActivity.java
+++ app/src/main/java/com/example/profile/FormActivity.java
@@ -1,14 +1,63 @@
 package com.example.profile;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FormActivity extends AppCompatActivity {
+
+    //  deklarasi variable
+    Button btnSimpan, btnHapus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
+
+        //  Inisialisasi variable
+        btnSimpan = findViewById(R.id.btnSimpan);
+        btnHapus = findViewById(R.id.btnHapus);
+
+        btnSimpan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(FormActivity.this, "Data Disimpan", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnHapus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(FormActivity.this);
+
+                // Membuat judul alert dialog
+                alertDialogBuilder.setTitle("Peringatan");
+                alertDialogBuilder.setMessage("Apakah anda yakin untuk menghapus data?")
+                        .setIcon(R.drawable.ic_avatar)
+                        .setCancelable(false)
+                        .setPositiveButton("Ya", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Toast.makeText(getApplicationContext(), "Data dihapus", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .setNegativeButton("Tidak", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.cancel();
+                            }
+                        });
+                // Membuat dialog
+                AlertDialog alertDialog = alertDialogBuilder.create();
+
+                // Menampilkan dialog
+                alertDialog.show();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/profile/MainActivity.java
+++ app/src/main/java/com/example/profile/MainActivity.java
@@ -3,12 +3,46 @@ package com.example.profile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView namaData, nimData;
+    EditText prodiData;
+    Button btnSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        namaData = findViewById(R.id.namaData);
+        nimData = findViewById(R.id.nimData);
+        prodiData = findViewById(R.id.prodiData);
+        btnSubmit = findViewById(R.id.btnSubmit);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    String name = namaData.getText().toString();
+                    String nim = nimData.getText().toString();
+                    String prodi = prodiData.getText().toString();
+
+                    if(prodi.isEmpty()) {
+                        Toast.makeText(getApplication(), "Program Studi Harus diisi!", Toast.LENGTH_SHORT).show();
+                    } else {
+                        
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Toast.makeText(getApplication(), "Error, TRY AGAIN!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
